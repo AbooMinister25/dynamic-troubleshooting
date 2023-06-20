3a5747fdcd3a5bf8e52d74d5394605ad88aba293
AbooMinister25 <aboominister@gmail.com>
2023-06-20 15:51:44 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+class Prompt extends HTMLElement {
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: "open" });
+        const template = document.getElementById("prompt-template").content;
+
+        let buttons = this.querySelectorAll(`.button`);
+
+        Array.from(buttons).forEach((button, idx) => {
+            button.addEventListener("click", () => {
+                let next = button.getAttribute("next");
+                let next_prompt = document.querySelector(`[name=${next}]`);
+
+                let level = Number(this.getAttribute("level"));
+                Array.from(
+                    document.querySelectorAll("interactive-prompt")
+                ).forEach((elem) => {
+                    let el_level = Number(elem.getAttribute("level"));
+                    if (el_level > level) {
+                        elem.classList.add("hidden");
+                    }
+                });
+
+                next_prompt.classList.remove("hidden");
+            });
+        });
+
+        this.shadowRoot.appendChild(template.cloneNode(true));
+    }
+}
+
+window.onload = () => {
+    window.customElements.define("interactive-prompt", Prompt);
+};
